6068bce471c60388c5922f3a94f96896295bf4f2
clarasionsaunier <clara.sion-saunier@epita.fr>
2025-11-20 14:27:58 +0100

    first commit

---
+++ src/main/java/org/example/projet_clara_sionsaunier/ProjetClaraSionSaunierApplication.java
@@ -0,0 +1,13 @@
+package org.example.projet_clara_sionsaunier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetClaraSionSaunierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetClaraSionSaunierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projet_clara_sionsaunier/ProjetClaraSionSaunierApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projet_clara_sionsaunier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetClaraSionSaunierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
